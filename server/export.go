b0192735b981fe834c8f6c48ab507523231221a5
Marvin Blum <marvin@marvinblum.de>
2019-06-14 15:26:39 +0200

    Fixed loading server list and exporting server configuration files.

--- server/export.go
+++ server/export.go
@@ -29,6 +29,10 @@ func ExportServer(id int, withPasswords bool) ([]byte, error) {
 		return nil, err
 	}
 
+	if err := addFileToZip(archive, server.Entrylist, entrylistJsonName); err != nil {
+		return nil, err
+	}
+
 	if err := archive.Close(); err != nil {
 		logrus.WithError(err).Error("Error closing zip file")
 		return nil, err
