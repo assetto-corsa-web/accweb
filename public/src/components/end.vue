0c823585fb8cd053e10a9c9969f611428df2b6d9
Jan <34112129+itsmylife44@users.noreply.github.com>
2021-11-28 14:34:43 +0100

    bump version to last

--- public/src/components/end.vue
+++ public/src/components/end.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="end">
-        <a href="https://github.com/assetto-corsa-web/accweb" target="_blank">accweb Version 1.13.4</a>
+        <a href="https://github.com/assetto-corsa-web/accweb" target="_blank">accweb Version 1.14.1</a>
     </div>
 </template>
 
