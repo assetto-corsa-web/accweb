0aaa3868effd26ba7adced8d6650638ce0e74988
Pedro Faria <pedro.faria@deliveryhero.com>
2021-12-07 01:18:05 +0100

    first trial

---
+++ internal/pkg/server/config.go
@@ -0,0 +1,145 @@
+package server
+
+import "time"
+
+type accConfigFiles struct {
+	Configuration ConfigurationJson
+	Settings      SettingsJson
+	Event         EventJson
+	EventRules    EventRulesJson
+	Entrylist     EntrylistJson
+	Bop           BopJson
+	AssistRules   AssistRulesJson
+}
+
+type AccWebConfigJson struct {
+	ID        int       `json:"id"`
+	Md5Sum    string    `json:"md5Sum"`
+	AutoStart bool      `json:"autoStart"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type ConfigurationJson struct {
+	ConfigVersion   int `json:"configVersion"`
+	UdpPort         int `json:"udpPort"`
+	TcpPort         int `json:"tcpPort"`
+	MaxConnections  int `json:"maxConnections"`
+	RegisterToLobby int `json:"registerToLobby"`
+	LanDiscovery    int `json:"lanDiscovery"`
+}
+
+type SettingsJson struct {
+	ConfigVersion              int    `json:"configVersion"`
+	ServerName                 string `json:"serverName"`
+	Password                   string `json:"password"`
+	AdminPassword              string `json:"adminPassword"`
+	SpectatorPassword          string `json:"spectatorPassword"`
+	TrackMedalsRequirement     int    `json:"trackMedalsRequirement"`
+	SafetyRatingRequirement    int    `json:"safetyRatingRequirement"`
+	RacecraftRatingRequirement int    `json:"racecraftRatingRequirement"`
+	IgnorePrematureDisconnects int    `json:"ignorePrematureDisconnects"`
+	DumpLeaderboards           int    `json:"dumpLeaderboards"`
+	IsRaceLocked               int    `json:"isRaceLocked"`
+	RandomizeTrackWhenEmpty    int    `json:"randomizeTrackWhenEmpty"`
+	MaxCarSlots                int    `json:"maxCarSlots"`
+	CentralEntryListPath       string `json:"centralEntryListPath"`
+	ShortFormationLap          int    `json:"shortFormationLap"`
+	AllowAutoDQ                int    `json:"allowAutoDQ"`
+	DumpEntryList              int    `json:"dumpEntryList"`
+	FormationLapType           int    `json:"formationLapType"`
+	CarGroup                   string `json:"carGroup"`
+}
+
+type EventJson struct {
+	ConfigVersion                 int               `json:"configVersion"`
+	Track                         string            `json:"track"`
+	PreRaceWaitingTimeSeconds     int               `json:"preRaceWaitingTimeSeconds"`
+	SessionOverTimeSeconds        int               `json:"sessionOverTimeSeconds"`
+	AmbientTemp                   int               `json:"ambientTemp"`
+	TrackTemp                     int               `json:"trackTemp,omitempty"`
+	CloudLevel                    float64           `json:"cloudLevel"`
+	Rain                          float64           `json:"rain"`
+	WeatherRandomness             int               `json:"weatherRandomness"`
+	Sessions                      []SessionSettings `json:"sessions"`
+	PostQualySeconds              int               `json:"postQualySeconds"`
+	PostRaceSeconds               int               `json:"postRaceSeconds"`
+	SimracerWeatherConditions     int               `json:"simracerWeatherConditions"`
+	IsFixedConditionQualification int               `json:"isFixedConditionQualification"`
+}
+
+type EventRulesJson struct {
+	ConfigVersion                        int  `json:"configVersion"`
+	QualifyStandingType                  int  `json:"qualifyStandingType"`
+	PitWindowLengthSec                   int  `json:"pitWindowLengthSec"`
+	DriverStintTimeSec                   int  `json:"driverStintTimeSec"`
+	MandatoryPitstopCount                int  `json:"mandatoryPitstopCount"`
+	MaxTotalDrivingTime                  int  `json:"maxTotalDrivingTime"`
+	MaxDriversCount                      int  `json:"maxDriversCount"`
+	IsRefuellingAllowedInRace            bool `json:"isRefuellingAllowedInRace"`
+	IsRefuellingTimeFixed                bool `json:"isRefuellingTimeFixed"`
+	IsMandatoryPitstopRefuellingRequired bool `json:"isMandatoryPitstopRefuellingRequired"`
+	IsMandatoryPitstopTyreChangeRequired bool `json:"isMandatoryPitstopTyreChangeRequired"`
+	IsMandatoryPitstopSwapDriverRequired bool `json:"isMandatoryPitstopSwapDriverRequired"`
+	TyreSetCount                         int  `json:"tyreSetCount"`
+}
+
+type SessionSettings struct {
+	HourOfDay              int    `json:"hourOfDay"`
+	DayOfWeekend           int    `json:"dayOfWeekend"`
+	TimeMultiplier         int    `json:"timeMultiplier"`
+	SessionType            string `json:"sessionType"`
+	SessionDurationMinutes int    `json:"sessionDurationMinutes"`
+}
+
+type EntrylistJson struct {
+	ConfigVersion  int             `json:"configVersion"`
+	Entries        []EntrySettings `json:"entries"`
+	ForceEntryList int             `json:"forceEntryList"`
+}
+
+type EntrySettings struct {
+	Drivers                      []DriverSettings `json:"drivers"`
+	RaceNumber                   int              `json:"raceNumber"`
+	ForcedCarModel               int              `json:"forcedCarModel"`
+	OverrideDriverInfo           int              `json:"overrideDriverInfo"`
+	IsServerAdmin                int              `json:"isServerAdmin"`
+	CustomCar                    string           `json:"customCar"`
+	OverrideCarModelForCustomCar int              `json:"overrideCarModelForCustomCar"`
+	BallastKg                    int              `json:"ballastKg"`
+	Restrictor                   int              `json:"restrictor"`
+	DefaultGridPosition          int              `json:"defaultGridPosition"`
+}
+
+type DriverSettings struct {
+	FirstName      string `json:"firstName"`
+	LastName       string `json:"lastName"`
+	ShortName      string `json:"shortName"`
+	DriverCategory int    `json:"driverCategory"`
+	PlayerID       string `json:"playerID"`
+}
+
+type BopJson struct {
+	ConfigVersion int           `json:"configVersion"`
+	Entries       []BopSettings `json:"entries"`
+}
+
+type BopSettings struct {
+	Track      string `json:"track"`
+	CarModel   int    `json:"carModel"`
+	BallastKg  int    `json:"ballastKg"`
+	Restrictor int    `json:"restrictor"`
+}
+
+type AssistRulesJson struct {
+	ConfigVersion            int `json:"configVersion"`
+	StabilityControlLevelMax int `json:"stabilityControlLevelMax"`
+	DisableAutosteer         int `json:"disableAutosteer"`
+	DisableAutoLights        int `json:"disableAutoLights"`
+	DisableAutoWiper         int `json:"disableAutoWiper"`
+	DisableAutoEngineStart   int `json:"disableAutoEngineStart"`
+	DisableAutoPitLimiter    int `json:"disableAutoPitLimiter"`
+	DisableAutoGear          int `json:"disableAutoGear"`
+	DisableAutoClutch        int `json:"disableAutoClutch"`
+	DisableIdealLine         int `json:"disableIdealLine"`
+}
