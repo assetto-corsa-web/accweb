5afe7229bd632eba3d039f2d6a003cf130043312
Mats Lindh <mats@lindh.no>
2022-03-21 21:37:55 +0100

    fix: convert layout for small values layout use css grids

--- public/src/components/server_config/settings.vue
+++ public/src/components/server_config/settings.vue
@@ -13,14 +13,18 @@
             <field v-if="!spectatorPasswordIsEmpty" type="password" :label="$t('spectatorpassword_label')" v-model="spectatorPassword" :disabled="spectatorPasswordIsEmpty"></field>
             <checkbox :label="$t('spectatorpassword_empty_label')" v-model="spectatorPasswordIsEmpty"></checkbox>
         </div>
-        <field type="number" :label="$t('trackmedalsrequirement_label')" v-model="trackMedalsRequirement"></field>
-        <field type="number" :label="$t('safetyratingrequirement_label')" v-model="safetyRatingRequirement"></field>
-        <field type="number" :label="$t('racecraftratingrequirement_label')" v-model="racecraftRatingRequirement"></field>
-        <field type="number" :label="$t('ignorePrematureDisconnects_label')" v-model="ignorePrematureDisconnects"></field>
+        <div class="server-settings-container three-columns" style="margin-top: 1.0rem;">
+            <field type="number" :label="$t('trackmedalsrequirement_label')" v-model="trackMedalsRequirement"></field>
+            <field type="number" :label="$t('safetyratingrequirement_label')" v-model="safetyRatingRequirement"></field>
+            <field type="number" :label="$t('racecraftratingrequirement_label')" v-model="racecraftRatingRequirement"></field>
+        </div>
+        <div class="server-settings-container two-columns">
+            <field type="number" :label="$t('maxcarslots_label')" v-model="maxCarSlots"></field>
+            <field type="number" :label="$t('ignorePrematureDisconnects_label')" v-model="ignorePrematureDisconnects"></field>
+        </div>
         <checkbox :label="$t('dumpleaderboards_label')" v-model="dumpLeaderboards"></checkbox>
         <checkbox :label="$t('isracelocked_label')" v-model="isRaceLocked"></checkbox>
         <checkbox :label="$t('randomizetrackwhenempty_label')" v-model="randomizeTrackWhenEmpty"></checkbox>
-        <field type="number" :label="$t('maxcarslots_label')" v-model="maxCarSlots"></field>
         <field type="text" :label="$t('centralentrylistpath_label')" v-model="centralEntryListPath"></field>
         <checkbox :label="$t('shortformationlap_label')" v-model="shortFormationLap"></checkbox>
         <checkbox :label="$t('allowautodq_label')" v-model="allowAutoDQ"></checkbox>
@@ -102,7 +106,7 @@ export default {
             data.passwordIsEmpty = data.password === "";
             data.adminPasswordIsEmpty = data.adminPassword === "";
             data.spectatorPasswordIsEmpty = data.spectatorPasswordIsEmpty === "";
-            
+
             this.setData(data);
         },
         getData() {
