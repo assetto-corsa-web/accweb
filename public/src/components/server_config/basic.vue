c9c0da7383c4d15ffab0c34e383a261761a0401f
Pedro Faria <predofaria@gmail.com>
2022-03-24 16:11:44 +0100

    feat: Add PublicIP field to configuration.json file

--- public/src/components/server_config/basic.vue
+++ public/src/components/server_config/basic.vue
@@ -1,8 +1,13 @@
 <template>
     <collapsible :title="$t('title')" with-import="true" import-filename="configuration.json" @load="setData">
-        <field type="number" :label="$t('udp_label')" v-model="udpPort"></field>
-        <field type="number" :label="$t('tcp_label')" v-model="tcpPort"></field>
-        <field type="number" :label="$t('maxconnections_label')" v-model="maxConnections"></field>
+        <div class="server-settings-container two-columns">
+            <field type="number" :label="$t('udp_label')" v-model="udpPort"></field>
+            <field type="number" :label="$t('tcp_label')" v-model="tcpPort"></field>
+        </div>
+        <div class="server-settings-container two-columns">
+            <field type="number" :label="$t('maxconnections_label')" v-model="maxConnections"></field>
+            <field :label="$t('publicip_label')" v-model="publicIP"></field>
+        </div>
         <checkbox :label="$t('registertolobby_label')" v-model="registerToLobby"></checkbox>
         <checkbox :label="$t('landiscovery_label')" v-model="lanDiscovery"></checkbox>
     </collapsible>
@@ -21,7 +26,8 @@ export default {
             tcpPort: 9600,
             maxConnections: 10,
             registerToLobby: true,
-            lanDiscovery: false
+            lanDiscovery: false,
+            publicIP: ""
         };
     },
     methods: {
@@ -31,6 +37,7 @@ export default {
             this.maxConnections = data.maxConnections;
             this.registerToLobby = data.registerToLobby;
             this.lanDiscovery = data.lanDiscovery;
+            this.publicIP = data.publicIP;
         },
         getData() {
             return {
@@ -38,7 +45,8 @@ export default {
                 tcpPort: parseInt(this.tcpPort),
                 maxConnections: parseInt(this.maxConnections),
                 registerToLobby: this.registerToLobby ? 1 : 0,
-                lanDiscovery: this.lanDiscovery ? 1 : 0
+                lanDiscovery: this.lanDiscovery ? 1 : 0,
+                publicIP: this.publicIP
             };
         }
     }
@@ -53,7 +61,8 @@ export default {
         "tcp_label": "TCP port",
         "maxconnections_label": "Max. connections",
         "registertolobby_label": "Register to lobby",
-        "landiscovery_label": "LAN Discovery"
+        "landiscovery_label": "LAN Discovery",
+        "publicip_label": "Public IP"
     }
 }
 </i18n>
