33f2762a8905f1d7df39d20585bee1d8d6f42d40
Pedro Faria <eu@eusouopedro.com>
2023-12-01 01:41:29 +0000

    feat: token timeout in configuration file

--- internal/pkg/cfg/cfg.go
+++ internal/pkg/cfg/cfg.go
@@ -1,7 +1,8 @@
 package cfg
 
 import (
-	"io/ioutil"
+	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
@@ -40,11 +41,12 @@ type CORS struct {
 }
 
 type Auth struct {
-	PublicKeyPath     string `yaml:"public_key_path"`
-	PrivateKeyPath    string `yaml:"private_key_path"`
-	AdminPassword     string `yaml:"admin_password"`
-	ModeratorPassword string `yaml:"moderator_password"`
-	ReadOnlyPassword  string `yaml:"read_only_password"`
+	PublicKeyPath     string         `yaml:"public_key_path"`
+	PrivateKeyPath    string         `yaml:"private_key_path"`
+	AdminPassword     string         `yaml:"admin_password"`
+	ModeratorPassword string         `yaml:"moderator_password"`
+	ReadOnlyPassword  string         `yaml:"read_only_password"`
+	Timeout           *time.Duration `yaml:"timeout"`
 }
 
 type ACC struct {
@@ -54,7 +56,7 @@ type ACC struct {
 
 // Load loads the application config from config.yml.
 func Load(file string) *Config {
-	data, err := ioutil.ReadFile(file)
+	data, err := os.ReadFile(file)
 	if err != nil {
 		logrus.WithError(err).Fatal("Error loading configuration file")
 	}
@@ -74,6 +76,11 @@ func Load(file string) *Config {
 
 	if config.Auth.PublicKeyPath == "" {
 		config.Auth.PublicKeyPath = "secrets/token.public"
+	}
+
+	if config.Auth.Timeout == nil {
+		m5 := 5 * time.Minute
+		config.Auth.Timeout = &m5
 	}
 
 	skipWine = config.SkipWine
